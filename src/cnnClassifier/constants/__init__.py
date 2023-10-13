01d7e9a75c45487107b7cf5aa24718162c76cef3
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2023-10-13 22:50:22 +0530

    mention the yaml file path

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+SECRET_FILE_PATH = Path("config/secrect.yaml") # secret.yaml file path
+CONFIG_FILE_PATH = Path("config/config.yaml") # config.yaml file path
+PARAMS_FILE_PATH = Path("params.yaml") # params.yaml file path
